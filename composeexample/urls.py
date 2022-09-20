1a8ad840fad520019c0904d63b233e916a1b9b0a
rijalnitesh <nitesh.rijal@compliancequest.com>
2022-09-20 17:08:41 +0545

    add urls.py

--- composeexample/urls.py
+++ composeexample/urls.py
@@ -2,8 +2,9 @@
 from django.contrib import admin
 from django.urls import path
 from mysite.views import main
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main)
-
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
